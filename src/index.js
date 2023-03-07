ae5cd31efac3cde2eafdc4ca972614f7cfca4b19
Farhad Towfighian <fartow@gmail.com>
2023-03-07 15:41:12 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
+const greetingMessage = <div>This is first message from Workshop!</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(greetingMessage);
